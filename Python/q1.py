dbc05099d9529ce7981908f2f068ea0a035ce308
RyuJiN04 <DhananjayPSingh3097@gmail.com>
2025-11-16 17:59:37 +0530

    comiit

--- Python/q1.py
+++ Python/q1.py
@@ -10,11 +10,15 @@ for i in range(1,5):
  inplis1 = int(input(f"Enter value {i} for list 1 :"))
  list1.append(inplis1)
 print(list1)
+set1 = set(list1)
 
 for i in range(1,5):
  inplis2 = int(input(f"Enter value {i} for list 2 :"))
  list2.append(inplis2)
 print(list2)
+set2 = set(list2)
 
 # Printing union of two lists : 
-print(list1+list2)
+union = set1.union(set2)
+list = list(union)
+print(f"The union of the lists is : {list}")
